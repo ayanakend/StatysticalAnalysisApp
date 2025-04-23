dac1cbe50a5c9b4f9f38b0d2aeb98b6e5e5e3ad6
aiana <ayakend05@gmail.com>
2025-04-23 11:35:23 +0300

    Revert "Добавлены: "roads_data.xlsx" - файл с данными по варианту задания, "RoadsAnalyzer.cs" класс для обработки информации согласно варианту задания"
    This reverts commit 2a9520c7fc4388f6fb1336194e00827c2745a36c.

--- Services/AnalyzerFactory.cs
+++ Services/AnalyzerFactory.cs
@@ -3,24 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using StatysticalAnalysisApp.Analyzers;
+
 namespace StatysticalAnalysisApp.Services
 {
-    public static class AnalyzerFactory
+    internal class AnalyzerFactory
     {
-        public static AnalyzerBase CreateAnalyzer(int variant)
-        {
-            switch (variant)
-            {
-                case 10:
-                 //return new InflationAnalyzer();
-                case 14:
-                    return new PopulationAnalyzer();
-                case 16:
-                    return new RoadsAnalyzer();
-                default:
-                    throw new ArgumentException("Неверный вариант");
-            }
-        }
     }
 }
