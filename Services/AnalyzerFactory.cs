03d4c056c78094fdbc6f2e810f7375f8a26478cb
aiana <ayakend05@gmail.com>
2025-04-23 13:28:55 +0300

    Заполнен класс "AnalyzerFactory" -  паттерн "Фабрика" для создания анализаторов по номеру варианта

--- Services/AnalyzerFactory.cs
+++ Services/AnalyzerFactory.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using StatysticalAnalysisApp.Analyzers;
 namespace StatysticalAnalysisApp.Services
 {
-    internal class AnalyzerFactory
+    public static class AnalyzerFactory
     {
+        public static AnalyzerBase CreateAnalyzer(int variant)
+        {
+            switch (variant)
+            {
+                case 10:
+                  //return new InflationAnalyzer();
+                case 14:
+                    return new PopulationAnalyzer();
+                case 16:
+                    return new RoadsAnalyzer();
+                default:
+                    throw new ArgumentException("Неверный вариант");
+            }
+        }
     }
 }
