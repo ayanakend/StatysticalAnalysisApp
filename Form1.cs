68bd69446c32c1679a08b712082e04d73372d8e3
Алина Жуйкова <zhuykovochka@gmail.com>
2025-04-24 08:11:19 +0300

    Добавила метод для обработки выбора варианта анализа.

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using StatysticalAnalysisApp.Services;
 
 namespace StatysticalAnalysisApp
 {
@@ -15,6 +16,12 @@ namespace StatysticalAnalysisApp
         public Form1()
         {
             InitializeComponent();
+            comboBoxDataset.Items.AddRange(new[] { "10: Инфляция", "14: Население", "16: Дороги" });
+            comboBoxDataset.SelectedIndexChanged += ComboBoxDataset_SelectedIndexChanged;
+        }
+        private void ComboBoxDataset_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadDataForSelectedVariant();
         }
     }
 }
